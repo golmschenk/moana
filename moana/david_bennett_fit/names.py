decb9ae3842f5191554b830ddde9e14041eb82b6
Greg Olmschenk <greg@olmschenk.com>
2021-10-28 19:37:14 -0400

    Move names into a single enum, of which the pieces are used in the specific enum

--- moana/david_bennett_fit/names.py
+++ moana/david_bennett_fit/names.py
@@ -12,12 +12,13 @@ class LensModelParameterNameElement(str):
         value = name
         return str.__new__(cls, value)
 
-    def __init__(self, name: str, david_bennett_name: Optional[str] = None):
+    def __init__(self, name: str, david_bennett_name: Optional[str] = None, latex_string: Optional[str] = None):
         self.name: str = name
         self.david_bennett_name: Optional[str] = david_bennett_name
+        self.latex_string: Optional[str] = latex_string
 
 
-class LensModelParameterNameEnum:
+class NameEnumBase:
     @classmethod
     def as_list(cls) -> List[LensModelParameterNameElement]:
         member_list = inspect.getmembers(cls)
@@ -31,9 +32,12 @@ class LensModelParameterNameEnum:
                 return element
         raise KeyError(f'david_bennett_name `{david_bennett_name}` not found in {cls}.')
 
+
+class NameEnum(NameEnumBase):
     INVERSE_EINSTEIN_CROSSING_TIME = LensModelParameterNameElement(name='inverse_einstein_crossing_time', david_bennett_name='1/t_E')
+    EINSTEIN_CROSS_TIME = LensModelParameterNameElement(name='einstein_crossing_time', latex_string=r't_\mathrm{E}')
     MINIMUM_APPROACH_TIME = LensModelParameterNameElement(name='minimum_approach_time', david_bennett_name='t0')
-    MINIMUM_APPROACH_DISTANCE = LensModelParameterNameElement(name='minimum_approach_distance', david_bennett_name='umin')
+    MINIMUM_ANGULAR_SEPARATION = LensModelParameterNameElement(name='minimum_angular_separation', david_bennett_name='umin')
     SECONDARY_SEPARATION = LensModelParameterNameElement(name='secondary_separation', david_bennett_name='sep')
     SECONDARY_THETA = LensModelParameterNameElement(name='secondary_theta', david_bennett_name='theta')
     SECONDARY_EPSILON = LensModelParameterNameElement(name='secondary_epsilon', david_bennett_name='eps1')
@@ -41,14 +45,8 @@ class LensModelParameterNameEnum:
     V_SEPARATION = LensModelParameterNameElement(name='v_separation', david_bennett_name='v_sep')
     T_STAR = LensModelParameterNameElement(name='t_star', david_bennett_name='Tstar')
     T_FIX = LensModelParameterNameElement(name='t_fix', david_bennett_name='t_fix')
-
-
-class BinaryLensModelParameterNameEnum(LensModelParameterNameEnum):
     PI_ER = LensModelParameterNameElement(name='pi_er', david_bennett_name='piEr')
     PI_ETH = LensModelParameterNameElement(name='pi_eth', david_bennett_name='pieth')
-
-
-class BinarySourceModelParameterNameEnum(LensModelParameterNameEnum):
     PI_EX = LensModelParameterNameElement(name='pi_ex', david_bennett_name='piEx')
     PI_EY = LensModelParameterNameElement(name='pi_ey', david_bennett_name='piEy')
     T_0_S_2 = LensModelParameterNameElement(name='t_0_s_2', david_bennett_name='t0s2')
@@ -69,3 +67,44 @@ class BinarySourceModelParameterNameEnum(LensModelParameterNameEnum):
     THEX_2_MIN = LensModelParameterNameElement(name='thex_2_min', david_bennett_name='thex2_min')
     THEX_2_MAX = LensModelParameterNameElement(name='thex_2_max', david_bennett_name='thex2_max')
     T_SBININV = LensModelParameterNameElement(name='t_sbininv', david_bennett_name='T_Sbininv')
+
+
+class LensModelParameterNameEnum(NameEnumBase):
+    INVERSE_EINSTEIN_CROSSING_TIME = NameEnum.INVERSE_EINSTEIN_CROSSING_TIME
+    MINIMUM_APPROACH_TIME = NameEnum.MINIMUM_APPROACH_TIME
+    MINIMUM_ANGULAR_SEPARATION = NameEnum.MINIMUM_ANGULAR_SEPARATION
+    SECONDARY_SEPARATION = NameEnum.SECONDARY_SEPARATION
+    SECONDARY_THETA = NameEnum.SECONDARY_THETA
+    SECONDARY_EPSILON = NameEnum.SECONDARY_EPSILON
+    INVERSE_T_BIN = NameEnum.INVERSE_T_BIN
+    V_SEPARATION = NameEnum.V_SEPARATION
+    T_STAR = NameEnum.T_STAR
+    T_FIX = NameEnum.T_FIX
+
+
+class BinaryLensModelParameterNameEnum(LensModelParameterNameEnum):
+    PI_ER = NameEnum.PI_ER
+    PI_ETH = NameEnum.PI_ETH
+
+
+class BinarySourceModelParameterNameEnum(LensModelParameterNameEnum):
+    PI_EX = NameEnum.PI_EX
+    PI_EY = NameEnum.PI_EY
+    T_0_S_2 = NameEnum.T_0_S_2
+    UMINS_2 = NameEnum.UMINS_2
+    F_2_R_I = NameEnum.F_2_R_I
+    F_2_MRPOW_I = NameEnum.F_2_MRPOW_I
+    F_2_R_V = NameEnum.F_2_R_V
+    DT_E_21 = NameEnum.DT_E_21
+    DTHETA = NameEnum.DTHETA
+    TSTAR_2 = NameEnum.TSTAR_2
+    F_2_KPOW_I = NameEnum.F_2_KPOW_I
+    TCCH_1_MIN = NameEnum.TCCH_1_MIN
+    TCCH_1_MAX = NameEnum.TCCH_1_MAX
+    TCCH_2_MIN = NameEnum.TCCH_2_MIN
+    TCCH_2_MAX = NameEnum.TCCH_2_MAX
+    THEX_1_MIN = NameEnum.THEX_1_MIN
+    THEX_1_MAX = NameEnum.THEX_1_MAX
+    THEX_2_MIN = NameEnum.THEX_2_MIN
+    THEX_2_MAX = NameEnum.THEX_2_MAX
+    T_SBININV = NameEnum.T_SBININV
