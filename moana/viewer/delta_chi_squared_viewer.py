22fe532593e293f6ecd9adde61ee9b32eda49b37
Greg Olmschenk <greg@olmschenk.com>
2022-08-10 17:38:03 -0400

    Add a total chi square plot generator

--- moana/viewer/delta_chi_squared_viewer.py
+++ moana/viewer/delta_chi_squared_viewer.py
@@ -1,7 +1,10 @@
 """
 Code for displaying the delta chi squared.
 """
+from typing import List
+
 import numpy as np
+import pandas as pd
 from bokeh.plotting import Figure
 
 from moana.david_bennett_fit.run import Run
@@ -11,7 +14,7 @@ from moana.viewer.color_mapper import ColorMapper
 
 class ChiSquaredViewer:
     @classmethod
-    def for_comparison_of_two_fit_models(cls, run0: Run, run1: Run) -> Figure:
+    def for_comparison_of_two_fit_models_per_instrument(cls, run0: Run, run1: Run) -> Figure:
         color_mapper = ColorMapper()
         figure = Figure(x_axis_label='Time (Offset HJD)', y_axis_label='Cumulative delta chi squared')
         light_curves0 = LightCurve.list_for_run_directory_with_residuals(run0.path)
@@ -31,6 +34,43 @@ class ChiSquaredViewer:
             figure.circle(x=light_curve0.data_frame[ColumnName.TIME__MICROLENSING_HJD.value],
                           y=cumulative_delta_chi_squared, color=instrument_color, legend_label=instrument_suffix,
                           fill_alpha=0.5)
+        figure.legend.location = 'top_left'
+        figure.legend.click_policy = 'hide'
+        return figure
+
+    @staticmethod
+    def create_single_light_curve_data_frame_for_light_curves(light_curves: List[LightCurve]) -> pd.DataFrame:
+        combined_light_curve_data_frame = pd.concat([light_curve.data_frame.copy() for light_curve in light_curves])
+        combined_light_curve_data_frame.sort_values(by=ColumnName.TIME__MICROLENSING_HJD.value, inplace=True)
+        return combined_light_curve_data_frame
+
+
+    @classmethod
+    def for_comparison_of_two_fit_models_total_chi_squared_value_using_(cls, run0: Run, run1: Run) -> Figure:
+        figure = Figure(x_axis_label='Time (Offset HJD)', y_axis_label='Cumulative delta chi squared')
+
+        light_curves0 = LightCurve.list_for_run_directory_with_residuals(run0.path)
+        run0_data_frame = pd.concat([light_curve.data_frame.copy() for light_curve in light_curves0])
+        light_curves1 = LightCurve.list_for_run_directory_with_residuals(run1.path)
+        run1_data_frame = pd.concat([light_curve.data_frame.copy() for light_curve in light_curves1])
+        merged_data_frame = run0_data_frame.merge(
+            run1_data_frame, how='outer', on=ColumnName.TIME__MICROLENSING_HJD.value, suffixes=('_run0', '_run1'))
+        merged_data_frame.sort_values(by=ColumnName.TIME__MICROLENSING_HJD.value, inplace=True)
+        cumulative_delta = 0
+        for index, row in merged_data_frame.iterrows():
+            if pd.isna(row[FitModelColumnName.CHI_SQUARED.value + '_run0']):
+                new_delta = row[FitModelColumnName.CHI_SQUARED.value + '_run1']
+            elif pd.isna(row[FitModelColumnName.CHI_SQUARED.value + '_run1']):
+                new_delta = -row[FitModelColumnName.CHI_SQUARED.value + '_run0']
+            else:
+                new_delta = row[FitModelColumnName.CHI_SQUARED.value + '_run1'] - row[FitModelColumnName.CHI_SQUARED.value + '_run0']
+            cumulative_delta += new_delta
+            merged_data_frame.loc[index, 'cumulative_delta_chi_squared'] = cumulative_delta
+        # TODO: Need to handle if the two values do not line up.
+        figure.line(x=merged_data_frame[ColumnName.TIME__MICROLENSING_HJD.value],
+                    y=merged_data_frame['cumulative_delta_chi_squared'], line_width=2)
+        figure.circle(x=merged_data_frame[ColumnName.TIME__MICROLENSING_HJD.value],
+                      y=merged_data_frame['cumulative_delta_chi_squared'], fill_alpha=0.5)
         figure.legend.location = 'top_left'
         figure.legend.click_policy = 'hide'
         return figure
